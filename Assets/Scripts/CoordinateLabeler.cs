d0f4dcc93e68cfdcb70835f0f353b99fd694b5ad
enesbahtiyar <enesbahtiyarserce@gmail.com>
2022-11-17 14:42:06 +0300

    Realm Rush Last edit for this time

--- Assets/Scripts/CoordinateLabeler.cs
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -32,6 +32,7 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
+            Label.enabled = true;
         }
 
         SetLabelColor();
